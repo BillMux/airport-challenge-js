5a06090d98f848951e5182a1d71f0d05dc1aab48
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-08 18:17:56 +0100

    Add mock plane spy obj

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,7 +1,20 @@
+// require('test-sweet')
+
 describe('Airport', function() {
-  var airport = new Airport
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = jasmine.createSpyObj('plane', ['land', 'takeOff'])
+  });
 
   it('has a hangar', function() {
-    expect(airport.hangar).toEqual(jasmine.any(Array))
+    expect(airport.hangar).toEqual([])
+  });
+
+  it('landed planes are stored in the hangar', function() {
+    airport.land(plane)
+    expect(airport.hangar).toEqual([plane])
   });
 });
